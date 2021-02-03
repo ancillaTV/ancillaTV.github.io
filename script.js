803753f9bbd401f762c6f6fc1f99bd14cf9b2bb4
ancillaTV <liamhallx@gmail.com>
2021-02-03 03:16:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let Reddit = 'https://www.reddit.com/'
+let Email = 'https://mail.google.com/mail/u/0/#inbox/'
+
+const BTN = document.querySelector('td');
+
+BTN.addEventListener('click', mylinks);
+
+function mylinks() {
+	
+	}
+	
+document.getElementbyId('Reddit').onclick = document.location.href = 'https://mail.google.com/mail/u/0/#inbox/';
+	
+
+onClick="document.location.href='https://www.twitch.tv/';"
+
